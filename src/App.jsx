c4d98be1acc113ebd41112ff15233edcb18b63f6
Arathy222003 <arathyyarya@gmail.com>
2025-11-04 15:44:15 +0530

    modified1

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,8 @@ function App() {
             <div className="flex justify-center items-center min-h-screen w-screen bg-gradient-to-br from-blue-100 to-orange-50">
               <QuoteForm />
             </div>
-             <div className="mt-10">
-            <ReadMore
-              text="We bring you handpicked holiday experiences that combine adventure, culture, and comfort. Our travel specialists ensure every trip is memorable — from luxurious getaways to budget-friendly escapes, crafted with care for every traveler."
-              maxLength={180}
-            />
-           </div>
-            <div className="flex justify-center items-center min-h-screen w-screen bg-gradient-to-br from-blue-100 to-orange-50">
-              <QuoteForm/>
-            </div>
+             
+            
 
         </div>
     );
